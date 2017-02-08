4b3a807f9675ee55772e181da1685d84edbbebed
louiehernandez95 <louieghernandez95@gmail.com>
2017-02-08 16:20:26 -0600

    added functions into person-detail

---
+++ Facility-management-system/Usage Detail/person-detail.java
@@ -0,0 +1,24 @@
+package Usage Detail;
+
+public class Address(){
+    private String City;
+    private String State;
+    private String ZipCode;
+    private String Country;
+    private String HomeAddress;
+
+}
+public class Phone(){
+    private String AreaCode;
+    private String ServiceProvider;
+    private String PhoneNumber;
+    private String CountryNumber;
+}
+public class Person() {
+    private String FirstName;
+    private String LastName;
+    private String Gender;
+    private String Sex;
+    private String Age;
+    private String DateOfBirth;
+    private String Email;
