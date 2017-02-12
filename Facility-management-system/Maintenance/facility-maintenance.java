c3efe9c51bf6677e3253a90d3193909a780dcc4c
adil <aahmad8@luc.edu>
2017-02-12 14:41:31 -0600

    Finished Maintenance Class. Includes Facility Maintenance, and Maintenance Request.

--- Facility-management-system/Maintenance/facility-maintenance.java
+++ Facility-management-system/Maintenance/facility-maintenance.java
@@ -6,28 +6,72 @@ import java.math.*;
 import java.sql.*;
 
 public class FacilityMaintenance {
-    public Date makeFacilityMaintRequest(Facility facility){
+
+    private String Id;
+    private String Type;
+    private double Cost;
+    private ArrayList<IFacility> facilities = new ArrayList<IFacility> ();
+
+    public Maintainance(){
+        this.Id = null;
+        this.Type = null;
+        this.Cost = 0.0;
+    }
+
+    public Maintainance(String id, String type, double cost){
+        this.Id = id;
+        this.Type = type;
+        this.Cost = cost;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public void setCost(double cost){
+        Cost = cost;
+    }
+
+    public double getCost(){
+        return Cost;
+    }
+
+    //Used All These Methods - ADIL A
+
+    //public Date makeFacilityMaintRequest(Facility facility){
 
     }
-    public Date scheduleMaintenance(Facility facility){
+    //public Date scheduleMaintenance(Facility facility){
 
     }
-    public Float calcMaintenanceCostForFacility(Facility facility){
+   // public Float calcMaintenanceCostForFacility(Facility facility){
 
     }
-    public Float calcProblemRateForFacility(Facility facility){
+    //public Float calcProblemRateForFacility(Facility facility){
 
     }
-    public Float calcDowntimeForFacility(Facility facility){
+    //public Float calcDowntimeForFacility(Facility facility){
 
     }
-    public Date[] listMaintRequests(Facility facility){
+    //public Date[] listMaintRequests(Facility facility){
 
     }
-    public Date[] listMaintenance(Facility facility){
+   // public Date[] listMaintenance(Facility facility){
 
     }
-    public String[] listFacilityProblems(Facility facility){
+    //public String[] listFacilityProblems(Facility facility){
 
     }
 }
---
+++ Facility-management-system/Maintenance/maintenance-request.java
@@ -0,0 +1,77 @@
+//import
+
+
+public MaintenanceRequest(){}
+
+public void makeFacilityMaintRequest(Maintainance maintainance, Facility1 facility) {
+        maintainanceRequests.put(maintainance, facility);
+        }
+
+public void calcMaintainanceCost(Facility1 facility) {
+        double cost = 0.0;
+        for (Entry<Facility1, ArrayList<Maintainance>> entry : scheduledMaintainances.entrySet()) {
+        if (entry.getKey() == facility) {
+        for (Maintainance m : entry.getValue()){
+        cost += m.getCost();}
+        }
+        }
+        System.out.println(Cost);
+}
+
+public void calcProblemRateForFacility(Facility1 facility) {
+        int problemrate = 0;
+        for (Entry<Maintainance, Facility1> entry : maintainanceRequests.entrySet()){
+        if (entry.getValue() == facility){
+        if (entry.getKey().getType() == "Gas")
+        problemrate = 1;
+        else if (entry.getKey().getType() == "Electric")
+        problemrate = 2;
+        else if (entry.getKey().getType() == "Heat")
+        problemrate = 3;
+        }
+        }
+        System.out.println(ProblemRate);
+}
+
+public void calcDownTimeForFacility(Facility1 facility) {
+    int problemrate = 0;
+    for (Entry<Manintainance, Facility1> entry : manintainanceRequests.entrySet()){
+        if (entry.getValue() == facility){
+            if (entry.getKey().getType() == "Tornado"
+        problemrate = 4;
+            else if (entry.getKey().getType() == "Hurricane" )
+                problemrate = 5;
+
+        }
+    }
+        System.out.println(DownTime);
+        }
+
+public void listFacilityProblems(Facility1 facility) {
+        for (Entry<IFacility, ArrayList<Maintainance>> entry : scheduledMaintainances.entrySet()){
+        if (entry.getKey() == facility){
+        for (Maintainance m : entry.getValue())
+        System.out.println(m.getType());
+        }
+        }
+}
+
+public void scheduleMaintainance(Facility1 facility, ArrayList<Maintainance> maintainance) {
+        scheduledMaintainances.put(facility,maintainance);
+        }
+
+public void listMaintainance() {
+        for (Entry<Facility1, ArrayList<Maintainance>> entry : scheduledMaintainances.entrySet()){
+        System.out.println("Facility: " + entry.getKey().getFacilityInfo() + " requesting maintainances in...");
+        for (Maintainance m : entry.getValue()){
+        System.out.print(m.getType() + ", ");
+        }
+        }
+}
+
+public void listMaintainanceRequest() {
+        for (Entry<Maintainance, Facility1> entry : maintainanceRequests.entrySet())
+        System.out.println("Maintainance types: " + entry.getKey().getType() + " made by the facility" + entry.getValue().getFacilityInfo());
+        }
+
+        }
